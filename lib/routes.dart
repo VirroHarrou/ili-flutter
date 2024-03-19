9dea973609e66f9c3c34c65c4fd3f31b63bae300
TagidickTagidick <49733518+TagidickTagidick@users.noreply.github.com>
2024-03-19 15:27:08 +0300

    add ar page ios

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,16 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:tavrida_flutter/layouts/ar_page/ar_page.dart';
 import 'package:tavrida_flutter/layouts/auth_page/auth_page_view.dart';
 import 'package:tavrida_flutter/layouts/forum_page/view.dart';
 import 'package:tavrida_flutter/layouts/home_page.dart';
 import 'package:tavrida_flutter/layouts/splash_screen_page.dart';
 import 'package:tavrida_flutter/layouts/qr_page/QRScanner.dart';
-import 'package:tavrida_flutter/widgets/ARCoreWidget.dart';
+import 'package:tavrida_flutter/repositories/views/model.dart';
+
+import 'layouts/ar_page/ar_page_ios.dart';
 
 final routes = {
   "/" : (context) => const SplashScreen(),
   "/auth" : (context) => const AuthPage(),
   "/ForumDetail" : (context) => const ForumDetailPage(),
-  "/ar_page" : (context) => ARPage(),
+  "/ar_page" : (context) => ARPageIOS(model: ModalRoute.of(context)?.settings.arguments as Model),//ARPage(),
   "/home" : (context) => const HomePage(),
   "/QR" : (context) => const QRPage(),
 };
