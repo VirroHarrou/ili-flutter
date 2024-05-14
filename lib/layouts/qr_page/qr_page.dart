066ec2bcdfeb20a5125ab25faf6e3726899568ba
VirroHarrou <81008781+VirroHarrou@users.noreply.github.com>
2024-05-14 18:34:55 +0300

    minor fixes fix qr_page.dart

--- lib/layouts/qr_page/qr_page.dart
+++ lib/layouts/qr_page/qr_page.dart
@@ -43,73 +43,91 @@ class _QRPageState extends State<QRPage> {
   Widget buildScanner({bool barCodeError = false, bool codeError = false}) {
     final qrViewSize = MediaQuery.of(context).size.width * 0.8;
     final theme = Theme.of(context);
-    return Scaffold(
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          QRView(
-            key: key,
-            onQRViewCreated: _onQRViewCreated,
-            overlay: QrScannerOverlayShape(
-              borderColor: barCodeError ? AppColors.red : Colors.grey,
-              borderRadius: 10,
-              cutOutSize: qrViewSize,
-              overlayColor: const Color(0xBB000000),
+    final isKeyboardOff = MediaQuery.of(context).viewInsets.bottom == 0;
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        body: Stack(
+          alignment: Alignment.center,
+          children: [
+            QRView(
+              key: key,
+              onQRViewCreated: _onQRViewCreated,
+              overlay: QrScannerOverlayShape(
+                borderColor: barCodeError ? AppColors.red : Colors.grey,
+                borderRadius: 10,
+                cutOutSize: qrViewSize,
+                overlayColor: const Color(0xBB000000),
+              ),
+              formatsAllowed: const [
+                BarcodeFormat.qrcode,
+              ],
             ),
-          ),
-          Align(
-            alignment: const Alignment(-.98, -.92),
-            child: Padding(
-              padding: const EdgeInsets.all(12),
-              child: FloatingActionButton(
-                onPressed: () => Navigator.pop(context),
-                shape: const CircleBorder(),
-                backgroundColor: AppColors.grey,
-                child: const Icon(Icons.arrow_back),
+            Positioned(
+              height: qrViewSize + 140,
+              width: qrViewSize,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40.0),
+                    child: Text("Наведите камеру на QR-код", style: theme.textTheme.bodyMedium,),
+                  ),
+                  Text(
+                    'Если вам нет 18, используйте приложение в присутствии родителей. Cледите за своим окружением, AR может искажать объекты.',
+                    textAlign: TextAlign.center,
+                    style: theme.textTheme.bodyMedium,
+                  ),
+                ],
               ),
             ),
-          ),
-          Positioned(
-            height: qrViewSize + 140,
-            width: qrViewSize,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 40.0),
-                  child: Text("Наведите камеру на QR-код", style: theme.textTheme.bodyMedium,),
+            Positioned.fill(
+              child: Container(
+                height: double.infinity,
+                color: isKeyboardOff ? Colors.transparent : Colors.black87,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Align(
+                      alignment: const Alignment(-.98, -.92),
+                      child: Padding(
+                        padding: const EdgeInsets.all(12),
+                        child: FloatingActionButton(
+                          onPressed: ()  => Navigator.pop(context),
+                          shape: const CircleBorder(),
+                          backgroundColor: AppColors.grey,
+                          child: const Icon(Icons.arrow_back),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      width: qrViewSize * 0.6,
+                      bottom: 20,
+                      child: Column(
+                        children: [
+                          Text("Введите код модели", style: theme.textTheme.bodyMedium,),
+                          const SizedBox(height: 8,),
+                          CodeDisplay(
+                            count: 4,
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Open Sans',
+                              color: codeError ? AppColors.red : AppColors.black,
+                            ),
+                            onComplete: (code) {
+                              bloc.add(QRCodeCompleteEvent(code: code, context: context));
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                Text(
-                  'Если вам нет 18, используйте приложение в присутствии родителей. Cледите за своим окружением, AR может искажать объекты.',
-                  textAlign: TextAlign.center,
-                  style: theme.textTheme.bodyMedium,
-                ),
-              ],
+              ),
             ),
-          ),
-          Positioned(
-            width: qrViewSize * 0.6,
-            bottom: 20,
-            child: Column(
-              children: [
-                Text("Введите код модели", style: theme.textTheme.bodyMedium,),
-                const SizedBox(height: 8,),
-                CodeDisplay(
-                  count: 4,
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w600,
-                    fontFamily: 'Open Sans',
-                    color: codeError ? AppColors.red : AppColors.black,
-                  ),
-                  onComplete: (code) {
-                    bloc.add(QRCodeCompleteEvent(code: code, context: context));
-                  },
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
