5425fab148804f5214b97d817531222daddd81a9
VirroHarrou <81008781+VirroHarrou@users.noreply.github.com>
2024-05-28 14:37:15 +0300

    minor refactoring

--- lib/layouts/qr_page/bloc/qr_bloc.dart
+++ lib/layouts/qr_page/bloc/qr_bloc.dart
@@ -10,24 +10,30 @@ part 'qr_state.dart';
 
 class QRBloc extends Bloc<QREvent, QRState> {
   final modelService = Injector.appInstance.get<ModelService>();
+  bool acceptScanData = true;
 
   QRBloc() : super(QRDefault()) {
 
     on<QRBarCodeFindEvent>((event, emit) async {
+      if (!acceptScanData) return;
+      acceptScanData = false;
       emit(QRDefault());
       final RegExp exp = RegExp(r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}');
       if (!exp.hasMatch(event.barCode)) {
         emit(QRBarCodeFailure());
+        acceptScanData = true;
         return;
       }
       var model = await modelService.getModelDetail(id: event.barCode);
       if (model == null) {
         emit(QRBarCodeFailure());
+        acceptScanData = true;
         return;
       }
       if (event.context.mounted) {
-        event.context.push(Routes.loadingPage, extra: model);
+        event.context.pushReplacement(Routes.loadingPage, extra: model);
       }
+      acceptScanData = true;
     });
 
     on<QRCodeCompleteEvent>((event, emit) async {
